3625edeff6d40f37a5c6a78978831ac8ac0a0eed
rAnto0 <dark_neo3000@mail.ru>
2026-03-02 14:42:59 +0300

    feat: added database.py
    - updated config.py

--- microservices/auth/app/core/config.py
+++ microservices/auth/app/core/config.py
@@ -16,6 +16,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str = ""
     DATABASE_SYNC_URL: str = ""
     DATABASE_TEST_URL: str = ""
+    DEBUG: bool = False
     SECRET_KEY: str = ""
     ALGORITHM: str = ""
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
