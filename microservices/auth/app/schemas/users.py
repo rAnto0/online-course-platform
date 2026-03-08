c09b4ba770b708adec151f4c8329a947dcb4b67a
rAnto0 <dark_neo3000@mail.ru>
2026-03-08 18:19:59 +0300

    feat(auth): implement JWT creation and decoding functions and add user schemas

---
+++ microservices/auth/app/schemas/users.py
@@ -0,0 +1,42 @@
+from typing import Annotated
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, Field, EmailStr
+
+from app.models.enums import UserRole
+
+
+class UserBase(BaseModel):
+    username: Annotated[
+        str, Field(..., min_length=3, max_length=50, description="Никнейм пользователя")
+    ]
+    email: Annotated[
+        EmailStr | None, Field(max_length=50, description="Email пользователя")
+    ] = None
+
+    model_config = {"str_strip_whitespace": True}
+
+
+class UserRead(UserBase):
+    id: UUID
+    created_at: datetime
+    role: UserRole
+
+    model_config = {"from_attributes": True}
+
+
+class UserCreate(UserBase):
+    password: Annotated[
+        str, Field(..., min_length=3, max_length=50, description="Пароль пользователя")
+    ]
+
+
+class RefreshRequest(BaseModel):
+    refresh_token: str
+
+
+class TokenInfo(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "Bearer"
